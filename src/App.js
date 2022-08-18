2a00a5ca6e6cdc53630fe1b2c0dd26871c12f4ca
bpeddapudi <82531209+bpeddapudi@users.noreply.github.com>
2022-08-18 00:22:44 -0700

    word matching logic

--- src/App.js
+++ src/App.js
@@ -1,45 +1,103 @@
 // import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 import Card from './components/Card';
 import randomWords from 'random-words';
 import Results from './components/Results';
-function App() {
-  const getWords = () => {
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      words: this.getWords(),
+      currentIndex: 0,
+      currentUserInput: '',
+      score: 0,
+    };
+  }
+
+  getWords = () => {
     let words = [];
-    randomWords(15).map((word)=> {
-      words.push(new Word(word,-2));
+    randomWords(15).map((word) => {
+      return words.push(new Word(word, -2));
     });
+    if (words.length)
+      words[0].status = -1;
     return words;
   }
-  // list of possible state variables
-  /**
-   * Words
-   * Score
-   */
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <div className="mt-4">
-              <Card words={getWords()} timer="1:00">
-              </Card>
-              <Results score={0}/>
-            </div>            
+
+  updateWords = () => {
+    this.setState({
+      words: this.getWords
+    })
+  }
+
+  nextWord = () => {
+    if (this.state.currentIndex <= 15) {
+      this.setState(
+        {
+          currentIndex: this.state.currentIndex + 1
+        }
+      )
+    } else {
+      this.updateWords();
+      this.setState(
+        {
+          currentIndex: 0
+        }
+      )
+    }
+  }
+  handleUserInput = (event) => {
+    const userInput = event.target.value.trim();
+    console.log(userInput);
+    let wordsFromState = this.state.words;
+    let expectedWord = wordsFromState[this.state.currentIndex];
+
+    console.log(userInput);
+    console.log(expectedWord.word);
+    if (userInput.length === expectedWord.word.length) {
+      expectedWord.status = 0;
+      console.log(expectedWord.status);
+      if (userInput === expectedWord.word) {
+        expectedWord.status = 1;
+        wordsFromState[this.state.currentIndex] = expectedWord;
+        this.setState({
+          score: this.state.scor + 1,
+          words: wordsFromState
+        });
+        console.log(this.state);
+        this.nextWord();
+      }
+      event.target.value = '';
+    }
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="mt-4">
+                <Card words={this.state.words} inputHandler={this.handleUserInput} timer="1:00">
+                </Card>
+                <Results score={0} />
+              </div>
+            </div>
           </div>
         </div>
+        <div className="wrapper">
+        </div>
       </div>
-      <div className="wrapper">
-      </div>
-    </div>
-  );
+    )
+  }
 }
 
 class Word {
-  constructor(word,status) {
+  constructor(word, status) {
     this.word = word;
     this.status = status;
   }
 }
-
-export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Card extends Component {
     render() {
-        console.log(this.props.words);
         return (
             <div className="card">
                 <div className="card-header d-flex flex-row flex-wrap">
                     {this.props.words.map((wordObj) => {
-                        return <h3 className="px-2" key={wordObj.word}>{wordObj.word}</h3>;
+                        return <h3 className="px-2" key={wordObj.word}>{wordObj.word + ' ' + wordObj.status}</h3>;
                     })}
                 </div>
                 <div className="card-body text-center">
                     <div className="card-title"></div>
-                    <p className="card-text">
+                    <div className="card-text">
                         <div className="input-group mb-3">
-                            <input type="text" className="form-control form-control-lg" placeholder="Start typing here...." aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                            <input type="text" className="form-control form-control-lg" placeholder="Start typing here...." onChange={this.props.inputHandler} />
                             <span className="input-group-text">{this.props.timer}</span>
                             <button className="btn btn-primary" type="button">Reset</button>
                         </div>
-                    </p>
+                    </div>
                 </div>
             </div>
         )
